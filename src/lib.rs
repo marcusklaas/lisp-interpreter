acba312cbf2fdae2421f6c3564452ed701718b44
Marcus Klaas <mail@marcusklaas.nl>
2017-07-12 19:40:13 -0400

    Make checking correct lisp easier

--- src/lib.rs
+++ src/lib.rs
@@ -88,6 +88,18 @@ mod tests {
         run_lisp_with_state(lit, &mut State::new())
     }
 
+    fn check_lisp_ok<'i, I>(commands: I, expected_out: &str) where I: IntoIterator<Item = &'i str> {
+        let mut state = State::new();
+        let mut last_ret_val = None;
+
+        for cmd in commands {
+            let expr = parse_lisp_string(cmd).unwrap();
+            last_ret_val = Some(evaluate_lisp_expr(&expr, &mut state).unwrap());
+        }
+
+        assert_eq!(expected_out, last_ret_val.unwrap().to_string());
+    }
+
     // TODO: add tests for function definition and evaluation.
 
     #[test]
@@ -104,24 +116,12 @@ mod tests {
 
     #[test]
     fn run_simple_lisp_addition() {
-        let lit = "(3 (+ 1 3) 0)";
-        let expected = Ok(LispValue::SubValue(vec![
-            LispValue::Integer(3),
-            LispValue::Integer(4),
-            LispValue::Integer(0),
-        ]));
-        let result = run_lisp(lit);
-
-        assert_eq!(expected, result);
+        check_lisp_ok(vec!["(3 (+ 1 3) 0)"], "(3 4 0)");
     }
 
     #[test]
     fn run_nested_evaluation() {
-        let lit = "(+ 10 (+ 1 10))";
-        let expected = Ok(LispValue::Integer(21));
-        let result = run_lisp(lit);
-
-        assert_eq!(expected, result);
+        check_lisp_ok(vec!["(+ 10 (+ 1 10))"], "21");
     }
 
     #[test]
@@ -166,51 +166,16 @@ mod tests {
 
     #[test]
     fn test_variable() {
-        let mut state = State::new();
-        state.set_variable("x", LispValue::Integer(5000));
-
-        let lit = "(+ x x)";
-        let expected = Ok(LispValue::Integer(10_000u64));
-        let result: Result<_, LispError> =
-            parse_lisp_string(lit).map_err(From::from).and_then(|ast| {
-                evaluate_lisp_expr(&ast, &mut state).map_err(From::from)
-            });
-
-        assert_eq!(expected, result);
-    }
-
-    #[test]
-    fn test_variable_setting() {
-        let mut state = State::new();
-        let lit = "(define x 5)";
-        run_lisp_with_state(lit, &mut state).unwrap();
-
-        let lit = "(+ x 7)";
-        let expected = Ok(LispValue::Integer(12));
-        let result = run_lisp_with_state(lit, &mut state);
-
-        assert_eq!(expected, result);
+        check_lisp_ok(vec!["(define x 5000)", "(+ x x)"], "10000");
     }
 
     #[test]
     fn test_variable_list() {
-        let mut state = State::new();
-        state.set_variable("x", LispValue::Integer(3));
-
-        let lit = "(x 1 (+ 1 x) 5)";
-        let expected = Ok(LispValue::SubValue(vec![
-            LispValue::Integer(3),
-            LispValue::Integer(1),
-            LispValue::Integer(4),
-            LispValue::Integer(5),
-        ]));
-        let result = run_lisp_with_state(lit, &mut state);
-
-        assert_eq!(expected, result);
+        check_lisp_ok(vec!["(define x 3)", "(x 1 (+ 1 x) 5)"], "(3 1 4 5)");
     }
 
     #[test]
     fn eval_empty_list() {
-        assert_eq!("()", run_lisp("()").unwrap().to_string());
+        check_lisp_ok(vec!["()"], "()");
     }
 }
